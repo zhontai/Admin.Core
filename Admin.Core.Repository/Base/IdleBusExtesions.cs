448b4a565e67ba3cf5e5d2531c59056837552caf
zhontai <361243234@qq.com>
2021-06-11 18:34:13 +0800

    增加注册Freesql实例空闲回收时间设置

--- Admin.Core.Repository/Base/IdleBusExtesions.cs
+++ Admin.Core.Repository/Base/IdleBusExtesions.cs
@@ -23,9 +23,7 @@ namespace Admin.Core.Repository
 
             if (appConfig.Tenant && user.DataIsolationType == DataIsolationType.OwnDb)
             {
-                var tenantName = "tenant_" + user.TenantId?.ToString();
-                ib.TryRegister(tenantName, () => CreateFreeSql(user, appConfig, serviceProvider));
-                return ib.Get(tenantName);
+                return RegisterAndGetFreeSql(ib, user, appConfig, serviceProvider);
             }
             else
             {
@@ -35,70 +33,80 @@ namespace Admin.Core.Repository
         }
 
         /// <summary>
-        /// 创建FreeSql实例
+        /// 注册并获得FreeSql实例
         /// </summary>
         /// <param name="user"></param>
         /// <param name="appConfig"></param>
         /// <param name="serviceProvider"></param>
         /// <returns></returns>
-        private static IFreeSql CreateFreeSql(IUser user, AppConfig appConfig, IServiceProvider serviceProvider)
+        private static IFreeSql RegisterAndGetFreeSql(IdleBus<IFreeSql> ib, IUser user, AppConfig appConfig, IServiceProvider serviceProvider)
         {
-            var dbConfig = serviceProvider.GetRequiredService<DbConfig>();
-
-            //查询租户数据库信息
-            var freeSql = serviceProvider.GetRequiredService<IFreeSql>();
-            var tenantRepository = freeSql.GetRepository<TenantEntity>();
-            var tenant = tenantRepository.Select.DisableGlobalFilter("Tenant").WhereDynamic(user.TenantId).ToOne(a => new { a.DbType, a.ConnectionString });
-
-            var freeSqlBuilder = new FreeSqlBuilder()
-                    .UseConnectionString(tenant.DbType.Value, tenant.ConnectionString)
-                    .UseAutoSyncStructure(false)
-                    .UseLazyLoading(false)
-                    .UseNoneCommandParameter(true);
-
-            #region 监听所有命令
-
-            if (dbConfig.MonitorCommand)
+            var tenantName = "tenant_" + user.TenantId?.ToString();
+            var exists = ib.Exists(tenantName);
+            if (!exists)
             {
-                freeSqlBuilder.UseMonitorCommand(cmd => { }, (cmd, traceLog) =>
+                var dbConfig = serviceProvider.GetRequiredService<DbConfig>();
+                //查询租户数据库信息
+                var freeSql = serviceProvider.GetRequiredService<IFreeSql>();
+                var tenantRepository = freeSql.GetRepository<TenantEntity>();
+                var tenant = tenantRepository.Select.DisableGlobalFilter("Tenant").WhereDynamic(user.TenantId).ToOne(a => new { a.DbType, a.ConnectionString, a.IdleTime });
+
+                var timeSpan = tenant.IdleTime.HasValue && tenant.IdleTime.Value > 0 ? TimeSpan.FromMinutes(tenant.IdleTime.Value) : TimeSpan.MaxValue;
+                ib.TryRegister(tenantName, () =>
                 {
-                    Console.WriteLine($"{cmd.CommandText}\r\n");
-                });
+                    var freeSqlBuilder = new FreeSqlBuilder()
+                        .UseConnectionString(tenant.DbType.Value, tenant.ConnectionString)
+                        .UseAutoSyncStructure(false)
+                        .UseLazyLoading(false)
+                        .UseNoneCommandParameter(true);
+
+                    #region 监听所有命令
+
+                    if (dbConfig.MonitorCommand)
+                    {
+                        freeSqlBuilder.UseMonitorCommand(cmd => { }, (cmd, traceLog) =>
+                        {
+                            Console.WriteLine($"{cmd.CommandText}\r\n");
+                        });
+                    }
+
+                    #endregion 监听所有命令
+
+                    var fsql = freeSqlBuilder.Build();
+                    fsql.GlobalFilter.Apply<IEntitySoftDelete>("SoftDelete", a => a.IsDeleted == false);
+
+                    //配置实体
+                    DbHelper.ConfigEntity(fsql, appConfig);
+
+                    #region 监听Curd操作
+
+                    if (dbConfig.Curd)
+                    {
+                        fsql.Aop.CurdBefore += (s, e) =>
+                        {
+                            Console.WriteLine($"{e.Sql}\r\n");
+                        };
+                    }
+
+                    #endregion 监听Curd操作
+
+                    #region 审计数据
+
+                    //计算服务器时间
+                    var serverTime = fsql.Select<DualEntity>().Limit(1).First(a => DateTime.UtcNow);
+                    var timeOffset = DateTime.UtcNow.Subtract(serverTime);
+                    fsql.Aop.AuditValue += (s, e) =>
+                    {
+                        DbHelper.AuditValue(e, timeOffset, user);
+                    };
+
+                    #endregion 审计数据
+
+                    return fsql;
+                }, timeSpan);
             }
 
-            #endregion 监听所有命令
-
-            var fsql = freeSqlBuilder.Build();
-            fsql.GlobalFilter.Apply<IEntitySoftDelete>("SoftDelete", a => a.IsDeleted == false);
-
-            //配置实体
-            DbHelper.ConfigEntity(fsql, appConfig);
-
-            #region 监听Curd操作
-
-            if (dbConfig.Curd)
-            {
-                fsql.Aop.CurdBefore += (s, e) =>
-                {
-                    Console.WriteLine($"{e.Sql}\r\n");
-                };
-            }
-
-            #endregion 监听Curd操作
-
-            #region 审计数据
-
-            //计算服务器时间
-            var serverTime = fsql.Select<DualEntity>().Limit(1).First(a => DateTime.UtcNow);
-            var timeOffset = DateTime.UtcNow.Subtract(serverTime);
-            fsql.Aop.AuditValue += (s, e) =>
-            {
-                DbHelper.AuditValue(e, timeOffset, user);
-            };
-
-            #endregion 审计数据
-
-            return fsql;
+            return ib.Get(tenantName);
         }
     }
 }
