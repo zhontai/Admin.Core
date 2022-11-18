3402dc550d1e30c3c1cef36ad9f479967a8731c7
zhontai <361243234@qq.com>
2022-11-18 16:06:03 +0800

    更新描述

--- src/platform/ZhonTai.Admin/Services/User/Dto/UserGetOutput.cs
+++ src/platform/ZhonTai.Admin/Services/User/Dto/UserGetOutput.cs
@@ -5,7 +5,7 @@ namespace ZhonTai.Admin.Services.User.Dto;
 public class UserGetOutput : UserUpdateInput
 {
     /// <summary>
-    /// 角色
+    /// 角色列表
     /// </summary>
     public ICollection<UserGetRoleDto> Roles { get; set; }
 
