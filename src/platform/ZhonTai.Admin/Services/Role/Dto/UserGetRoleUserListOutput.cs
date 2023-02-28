ac29d0c51de4687dc7c3b9e2d2323a61f169ab1c
zhontai <361243234@qq.com>
2023-02-28 18:50:19 +0800

    新增角色用户列表手机号，升级包版本

--- src/platform/ZhonTai.Admin/Services/Role/Dto/UserGetRoleUserListOutput.cs
+++ src/platform/ZhonTai.Admin/Services/Role/Dto/UserGetRoleUserListOutput.cs
@@ -11,4 +11,9 @@ public class UserGetRoleUserListOutput
     /// 姓名
     /// </summary>
     public string Name { get; set; }
+
+    /// <summary>
+    /// 手机号
+    /// </summary>
+    public string Mobile { get; set; }
 }
