5348955472a64c9611fe5e6ca281717fb72f5e5d
zhontai <361243234@qq.com>
2023-04-20 17:29:43 +0800

    新增 appconfig.json.swagger.enableSchemaId启用SchemaId命名空间NamespaceassemblyNameList程序集列表，用于启用SchemaId命名空间，主要用于对接第三方nuget包输入输出实体生成swagger.json可能异常的问题 新增 SchemaIdAttribute特性自定义实体SchemaId，同时支持前缀名+modelName+后缀名

---
+++ src/platform/ZhonTai.Admin/Core/Attributes/SchemaIdAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ZhonTai.Admin.Core.Attributes;
+
+/// <summary>
+/// SchemaId
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true)]
+public class SchemaIdAttribute : Attribute
+{
+    /// <summary>
+    /// 前缀
+    /// </summary>
+    public string Prefix { get; set; }
+
+    /// <summary>
+    /// 后缀
+    /// </summary>
+    public string Suffix { get; set; }
+
+    /// <summary>
+    /// SchemaId
+    /// </summary>
+    public string SchemaId { get; set; }
+
+    public SchemaIdAttribute()
+    {
+    }
+
+    public SchemaIdAttribute(string schemaId)
+    {
+        SchemaId = schemaId;
+    }
+}
