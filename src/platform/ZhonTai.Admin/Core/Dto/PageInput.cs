787e0b8140689ce3cdeda00018098af3dbca4120
zhontai <361243234@qq.com>
2023-07-08 09:18:01 +0800

    优化分页当前页标和每页大小数据约束

--- src/platform/ZhonTai.Admin/Core/Dto/PageInput.cs
+++ src/platform/ZhonTai.Admin/Core/Dto/PageInput.cs
@@ -5,15 +5,31 @@
 /// </summary>
 public class PageInput
 {
+    private int _currentPage;
+    private int _pageSize;
+
     /// <summary>
     /// 当前页标
     /// </summary>
-    public virtual int CurrentPage { get; set; } = 1;
+    public virtual int CurrentPage 
+    {
+        get => _currentPage < 1 ? 1 : _currentPage;
+        set => _currentPage = value;
+    }
 
     /// <summary>
     /// 每页大小
     /// </summary>
-    public virtual int PageSize { set; get; } = 50;
+    public virtual int PageSize 
+    {
+        get
+        {
+            if (_pageSize < 1) _pageSize = 1;
+            if (_pageSize > 1000) _pageSize = 1000;
+            return _pageSize;
+        }
+        set => _pageSize = value;
+    }
 
     /// <summary>
     /// 高级查询条件
