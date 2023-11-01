83a9ab0b304ba064072f71e4e3f87c899f9578c0
zhontai <361243234@qq.com>
2023-11-01 11:32:20 +0800

    新增用户密码格式验证，避免简单密码设置

---
+++ src/platform/ZhonTai.Common/Helpers/PasswordHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ZhonTai.Common.Helpers;
+
+/// <summary>
+/// 密码帮助类
+/// </summary>
+public partial class PasswordHelper
+{
+   
+    [GeneratedRegex(@"^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d!@#$%^&.*]{6,16}$")]
+    public static partial Regex RegexPassword();
+
+    /// <summary>
+    /// 验证
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static bool Verify(string input)
+    {
+        if (input.IsNull()) { 
+            return false; 
+        }
+
+        return RegexPassword().IsMatch(input);
+    }
+}
