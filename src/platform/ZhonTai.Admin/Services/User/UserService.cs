51e9e07c26bc6aa7629ca8f3f2e0c258a0f14acd
zhontai <361243234@qq.com>
2024-01-02 14:17:29 +0800

    删除测试

--- src/platform/ZhonTai.Admin/Services/User/UserService.cs
+++ src/platform/ZhonTai.Admin/Services/User/UserService.cs
@@ -148,8 +148,6 @@ public partial class UserService : BaseService, IUserService, IDynamicApi
             }
         }
 
-        throw ResultOutput.Exception("测试异常", "100", 500);
-
         var data = new PageOutput<UserGetPageOutput>()
         {
             List = Mapper.Map<List<UserGetPageOutput>>(list),
