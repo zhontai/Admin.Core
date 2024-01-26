9572e5904a0bee4b9c2f45499ec9f22f037a759b
zhontai <361243234@qq.com>
2024-01-26 18:06:18 +0800

    更新项目模板任务调度

--- templates/admin/content/src/hosts/MyApp.Host/Program.cs
+++ templates/admin/content/src/hosts/MyApp.Host/Program.cs
@@ -31,6 +31,13 @@ using ZhonTai.Admin.Core.Db;
 using ZhonTai.Admin.Domain;
 using System;
 using Cronos;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using ZhonTai.Admin.Services.Msg;
+using ZhonTai.Admin.Services.TaskScheduler;
+using ZhonTai.Admin.Services.Msg.Events;
+using Mapster;
 #endif
 #if (!NoTaskScheduler)
 
@@ -98,7 +105,116 @@ new HostApp(new HostAppOptions()
                 freeSchedulerBuilder
                 .OnExecuting(task =>
                 {
-                    //执行任务
+                    var taskSchedulerConfig = AppInfo.GetRequiredService<IOptions<TaskSchedulerConfig>>().Value;
+
+                    if (task.Topic?.StartsWith("[system]shell") == true)
+                    {
+                        var jsonArgs = JToken.Parse(task.Body);
+                        var shellArgs = jsonArgs.Adapt<ShellArgs>();
+
+                        var arguments = shellArgs.Arguments;
+                        var modeulName = jsonArgs["moduleName"]?.ToString();
+                        if (modeulName.NotNull())
+                        {
+                            //通过moduleName获取配置文件moduleName对应的Grpc远程地址
+                            var grpcAddress = string.Empty;
+                            if (grpcAddress.NotNull())
+                            {
+                                arguments = arguments.Replace("${grpcAddress}", grpcAddress, StringComparison.OrdinalIgnoreCase);
+                            }
+                        }
+
+                        var fileName = shellArgs.FileName;
+                        if (fileName.IsNull())
+                        {
+                            fileName = taskSchedulerConfig?.ProcessStartInfo?.FileName;
+                        }
+
+                        var workingDirectory = shellArgs.WorkingDirectory;
+                        if (workingDirectory.IsNull())
+                        {
+                            workingDirectory = taskSchedulerConfig?.ProcessStartInfo?.WorkingDirectory;
+                        }
+
+                        var startInfo = new ProcessStartInfo
+                        {
+                            FileName = fileName,
+                            Arguments = arguments,
+                            UseShellExecute = false,
+                            RedirectStandardOutput = true,
+                            RedirectStandardError = true,
+                            WorkingDirectory = workingDirectory
+                        };
+
+                        var response = string.Empty;
+                        var error = string.Empty;
+                        using (var process = Process.Start(startInfo))
+                        {
+                            response = process.StandardOutput.ReadToEnd();
+                            error = process.StandardError.ReadToEnd();
+
+                            //if (response.NotNull())
+                            //{
+                            //    Console.WriteLine("Response:");
+                            //    Console.WriteLine(response);
+                            //}
+
+                            //if (error.NotNull())
+                            //{
+                            //    Console.WriteLine("Error:");
+                            //    Console.WriteLine(error);
+                            //}
+
+                            process.WaitForExit();
+                        }
+
+                        if (response.NotNull())
+                            task.Remark(response);
+
+                        if (error.NotNull())
+                            throw new Exception(error);
+                    }
+                })
+                .OnExecuted((task, taskLog) =>
+                {
+                    try
+                    {
+                        if (!taskLog.Success)
+                        {
+                            //发送告警邮件
+                            var taskService = AppInfo.GetRequiredService<TaskService>();
+                            var emailService = AppInfo.GetRequiredService<EmailService>();
+                            var alerEmail = taskService.GetAlerEmailAsync(task.Id).Result;
+                            var topic = task.Topic;
+                            if (alerEmail.NotNull())
+                            {
+                                var jsonArgs = JToken.Parse(task.Body);
+                                var desc = jsonArgs["desc"]?.ToString();
+                                if (desc.NotNull())
+                                    topic = desc;
+                            }
+                            alerEmail?.Split(',')?.ToList()?.ForEach(async address =>
+                            {
+                                await emailService.SingleSendAsync(new EamilSingleSendEvent
+                                {
+                                    ToEmail = new EamilSingleSendEvent.Models.EmailModel
+                                    {
+                                        Address = address,
+                                        Name = address
+                                    },
+                                    Subject = "【任务调度中心】监控报警",
+                                    Body = $@"<p>任务名称：{topic}</p>
+<p>任务编号：{task.Id}</p>
+<p>告警类型：调度失败</p>
+<p>告警内容：<br/>{taskLog.Exception}</p>"
+                                });
+                            });
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        AppInfo.Log.Error(ex);
+                    }
                 })
                 .UseCustomInterval(task =>
                 {
