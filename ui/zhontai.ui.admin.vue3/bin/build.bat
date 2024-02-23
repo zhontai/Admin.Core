9e902513b850b2b66ec17007e7c84afc4650946e
zhontai <361243234@qq.com>
2024-02-23 15:40:03 +0800

    包含前端bin文件

---
+++ ui/zhontai.ui.admin.vue3/bin/build.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+echo.
+echo 发布网站，生成dist文件
+echo.
+
+%~d0
+cd %~dp0
+
+cd ..
+npm run build
+
+pause
---
+++ ui/zhontai.ui.admin.vue3/bin/run-web.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+echo.
+echo 运行网站
+echo.
+
+%~d0
+cd %~dp0
+
+cd ..
+npm run dev
+
+pause
