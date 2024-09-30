26b95e7b5e638a68fce78911909efb570ceae998
zhontai <361243234@qq.com>
2024-09-30 11:57:13 +0800

    修复有固定选项卡，没有选择固定选项卡显示的问题 修复点击选项卡个人中心不能隐藏二级分栏的问题

--- ui/zhontai.ui.admin.vue3/src/layout/navBars/tagsView/tagsView.vue
+++ ui/zhontai.ui.admin.vue3/src/layout/navBars/tagsView/tagsView.vue
@@ -278,10 +278,10 @@ const closeCurrentTagsView = (path: string) => {
       }
     }
   })
-  if(state.tagsViewList===[]|| state.tagsViewList.length===0)
-  router.push('/')
 
   addBrowserSetSession(state.tagsViewList)
+
+  router.push({ path: state.tagsViewList.length > 0 ? state.tagsViewList[state.tagsViewList.length - 1].path : '/' })
 }
 // 4、关闭其它 tagsView：如果是设置了固定的（isAffix），不进行关闭
 const closeOtherTagsView = (path: string) => {
@@ -307,11 +307,10 @@ const closeAllTagsView = () => {
       if (v.meta?.isAffix && !v.meta.isHide) {
         v.url = setTagsViewHighlight(v)
         state.tagsViewList.push({ ...v })
-        router.push({ path: state.tagsViewList[state.tagsViewList.length - 1].path })
       }
     })
     addBrowserSetSession(state.tagsViewList)
-
+    router.push({ path: state.tagsViewList.length > 0 ? state.tagsViewList[state.tagsViewList.length - 1].path : '/' })
   }
 }
 // 6、开启当前页面全屏
@@ -361,10 +360,8 @@ const onCurrentContextmenuClick = async (item: RouteItem) => {
       closeOtherTagsView(path)
       break
     case 3:
-    
       // 关闭全部
       closeAllTagsView()
-      router.push('/')
       break
     case 4:
       // 开启当前页面全屏
@@ -393,7 +390,7 @@ const onTagsClick = (v: RouteItem, k: number) => {
   // 分栏布局时，收起/展开菜单
   if (getThemeConfig.value.layout === 'columns') {
     const item: RouteItem = routesList.value.find((r: RouteItem) => r.path.indexOf(`/${v.path.split('/')[1]}`) > -1)
-    !item.children ? (getThemeConfig.value.isCollapse = true) : (getThemeConfig.value.isCollapse = false)
+    !item.children || item.children?.length === 0 ? (getThemeConfig.value.isCollapse = true) : (getThemeConfig.value.isCollapse = false)
   }
   // if (getThemeConfig.value.layout === 'columns') {
   //   const item: RouteItem = routesList.value.find((r: RouteItem) => r.path.indexOf(`/${v.path.split('/')[1]}`) > -1)
