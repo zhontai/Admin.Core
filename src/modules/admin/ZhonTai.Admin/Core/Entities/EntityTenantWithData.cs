c6606b4ea555f7d648c3cda8c176af90cd2acd38
zhontai <361243234@qq.com>
2024-10-15 21:03:49 +0800

    更新实体属性顺序

--- src/modules/admin/ZhonTai.Admin/Core/Entities/EntityTenantWithData.cs
+++ src/modules/admin/ZhonTai.Admin/Core/Entities/EntityTenantWithData.cs
@@ -12,20 +12,21 @@ public class EntityTenantWithData<TKey> : EntityTenant<TKey>, IData where TKey :
     /// 拥有者Id
     /// </summary>
     [Description("拥有者Id")]
-    [Column(Position = -41)]
+    [Column(Position = -42)]
     public virtual long? OwnerId { get; set; }
 
     /// <summary>
     /// 拥有者部门Id
     /// </summary>
     [Description("拥有者部门Id")]
-    [Column(Position = -40)]
+    [Column(Position = -41)]
     public virtual long? OwnerOrgId { get; set; }
+
     /// <summary>
     /// 拥有者部门名称
     /// </summary>
     [Description("拥有者部门名称")]
-    [Column(Position = -39)]
+    [Column(Position = -40)]
     public virtual string? OwnerOrgName { get; set; }
 }
 
