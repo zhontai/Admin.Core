c8c0505d993bd396599e949b417999b2faa0c6bf
zhontai <361243234@qq.com>
2024-10-22 11:13:23 +0800

    优化添加任务调度代码，封装ConfigureScheduler、ExecutGrpc、FailRetry、SendAlarmEmail系统方法按需引用

--- src/modules/admin/ZhonTai.Host/Program.cs
+++ src/modules/admin/ZhonTai.Host/Program.cs
@@ -1,5 +1,4 @@
-﻿using Cronos;
-using FreeScheduler;
+﻿using FreeScheduler;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
