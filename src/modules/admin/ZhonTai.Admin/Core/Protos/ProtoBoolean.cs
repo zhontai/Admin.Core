101ba1c997dc126a70384071ae3e21b755ef303c
zhontai <361243234@qq.com>
2025-01-10 12:13:17 +0800

    新增bool、int、long、string、DateTime、List常用Grpc类型封装

---
+++ src/modules/admin/ZhonTai.Admin/Core/Protos/ProtoBoolean.cs
@@ -0,0 +1,29 @@
+﻿using ProtoBuf;
+
+namespace ZhonTai.Admin.Core.Protos;
+
+/// <summary>
+/// ProtoBoolean 表示 Grpc 请求或响应中的 bool
+/// </summary>
+[ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+public class ProtoBoolean
+{
+    public bool Value { get; set; }
+
+    public ProtoBoolean() { }
+
+    public ProtoBoolean(bool value)
+    {
+        Value = value;
+    }
+
+    public static implicit operator ProtoBoolean(bool value)
+    {
+        return new ProtoBoolean(value);
+    }
+
+    public static implicit operator bool(ProtoBoolean result)
+    {
+        return result.Value;
+    }
+}
---
+++ src/modules/admin/ZhonTai.Admin/Core/Protos/ProtoLong.cs
@@ -0,0 +1,29 @@
+﻿using ProtoBuf;
+
+namespace ZhonTai.Admin.Core.Protos;
+
+/// <summary>
+/// ProtoLong 表示 Grpc 请求或响应中的 long
+/// </summary>
+[ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+public class ProtoLong
+{
+    public long Value { get; set; }
+
+    public ProtoLong() { }
+
+    public ProtoLong(long value)
+    {
+        Value = value;
+    }
+
+    public static implicit operator ProtoLong(long value)
+    {
+        return new ProtoLong(value);
+    }
+
+    public static implicit operator long(ProtoLong result)
+    {
+        return result.Value;
+    }
+}
