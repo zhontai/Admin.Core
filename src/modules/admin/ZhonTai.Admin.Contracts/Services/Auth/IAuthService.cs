2687ab1152d525cdcc898333ea8dc3e77d1337a8
zhontai <361243234@qq.com>
2025-01-21 21:33:37 +0800

    增加权限客户端服务

--- src/modules/admin/ZhonTai.Admin.Contracts/Services/Auth/IAuthService.cs
+++ src/modules/admin/ZhonTai.Admin.Contracts/Services/Auth/IAuthService.cs
@@ -1,22 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Refit;
-using System.Threading.Tasks;
-using ZhonTai.Admin.Core.Attributes;
 using ZhonTai.Admin.Core.Auth;
-using ZhonTai.Admin.Core.Consts;
 using ZhonTai.Admin.Services.Auth.Dto;
 
 namespace ZhonTai.Admin.Services.Auth;
-
-/// <summary>
-/// 认证授权客户端接口
-/// </summary>
-[HttpClientContract(AdminConsts.AreaName)]
-public interface IAuthClientService
-{
-    [Post("/api/admin/auth/login")]
-    Task<TokenInfo> LoginAsync(AuthLoginInput input);
-}
 
 /// <summary>
 /// 认证授权接口
@@ -32,5 +18,4 @@ public interface IAuthService: IAuthClientService
     Task<AuthGetPasswordEncryptKeyOutput> GetPasswordEncryptKeyAsync();
 
     Task<TokenInfo> Refresh([BindRequired] string token);
-
 }
