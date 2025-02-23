794a749560a51b4fc09dc02a58c0c8e21c8e2a5a
zhontai <361243234@qq.com>
2025-02-23 21:41:21 +0800

    app项目模板CacheKeys缓存键增加获取模块缓存键方法

--- templates/app/content/MyApp.Api/Core/Consts/CacheKeys.cs
+++ templates/app/content/MyApp.Api/Core/Consts/CacheKeys.cs
@@ -12,4 +12,11 @@ public static partial class CacheKeys
     /// </summary>
     [Description("模块缓存键")]
     public const string ModuleActionKey = "module:action:";
+
+    /// <summary>
+    /// 获取模块缓存键
+    /// </summary>
+    /// <param name="id">模块Id</param>
+    /// <returns></returns>
+    public static string GetModuleActionKey(long id) => $"{ModuleActionKey}{id}";
 }
