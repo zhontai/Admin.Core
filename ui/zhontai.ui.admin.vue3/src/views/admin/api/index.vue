7506e601f3730d39e2d225b892fb96de5391df0e
Saber.Nagato <saber.nagato@outlook.com>
2025-03-02 22:48:26 +0800

    修复: 将 swaggerResourcePaths 从 'doc/admin' 改为 'doc/sys' 以匹配接口前缀

--- ui/zhontai.ui.admin.vue3/src/views/admin/api/index.vue
+++ ui/zhontai.ui.admin.vue3/src/views/admin/api/index.vue
@@ -332,7 +332,7 @@ const syncApi = async (swaggerResource: any) => {
 
 const onSync = async () => {
   state.syncLoading = true
-  const swaggerResourcePaths = ['doc/admin']
+  const swaggerResourcePaths = ['doc/sys']
   // const swaggerResourcePaths = ['doc/app']
   const swaggerResourceUrls = swaggerResourcePaths?.map((path) => `/${path}/swagger-resources`) as string[]
   const lastSwaggerResourcesIndex = swaggerResourceUrls.length - 1
