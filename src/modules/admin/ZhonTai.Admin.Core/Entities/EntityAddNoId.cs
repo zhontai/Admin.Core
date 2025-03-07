e5bcb1b4675c782b31b81b09a156431978266a62
zhontai <361243234@qq.com>
2025-03-07 17:11:43 +0800

    新增 基础实体类型：EntityUpdateNoId（实体修改无主键Id），EntityAddNoId（实体创建无主键Id）

---
+++ src/modules/admin/ZhonTai.Admin.Core/Entities/EntityAddNoId.cs
@@ -0,0 +1,41 @@
+﻿using FreeSql.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using ZhonTai.Admin.Core.Attributes;
+
+namespace ZhonTai.Admin.Core.Entities;
+
+/// <summary>
+/// 实体创建无主键Id
+/// </summary>
+public class EntityAddNoId : IEntityAdd
+{
+    /// <summary>
+    /// 创建者用户Id
+    /// </summary>
+    [Description("创建者用户Id")]
+    [Column(Position = -23, CanUpdate = false)]
+    public virtual long? CreatedUserId { get; set; }
+
+    /// <summary>
+    /// 创建者用户名
+    /// </summary>
+    [Description("创建者用户名")]
+    [Column(Position = -22, CanUpdate = false), MaxLength(60)]
+    public virtual string CreatedUserName { get; set; }
+
+    /// <summary>
+    /// 创建者姓名
+    /// </summary>
+    [Description("创建者姓名")]
+    [Column(Position = -21, CanUpdate = false), MaxLength(60)]
+    public virtual string CreatedUserRealName { get; set; }
+
+    /// <summary>
+    /// 创建时间
+    /// </summary>
+    [Description("创建时间")]
+    [Column(Position = -20, CanUpdate = false)]
+    [ServerTime]
+    public virtual DateTime? CreatedTime { get; set; }
+}
---
+++ src/modules/admin/ZhonTai.Admin.Core/Entities/EntityUpdateNoId.cs
@@ -0,0 +1,51 @@
+﻿using FreeSql.DataAnnotations;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using ZhonTai.Admin.Core.Attributes;
+
+namespace ZhonTai.Admin.Core.Entities;
+
+/// <summary>
+/// 实体修改无主键Id
+/// </summary>
+public class EntityUpdateNoId : EntityAddNoId, IEntityUpdate
+{
+    /// <summary>
+    /// 修改者用户Id
+    /// </summary>
+    [Description("修改者用户Id")]
+    [Column(Position = -13)]
+    [JsonProperty(Order = 10000)]
+    [JsonPropertyOrder(10000)]
+    public virtual long? ModifiedUserId { get; set; }
+
+    /// <summary>
+    /// 修改者用户名
+    /// </summary>
+    [Description("修改者用户名")]
+    [Column(Position = -12), MaxLength(60)]
+    [JsonProperty(Order = 10001)]
+    [JsonPropertyOrder(10001)]
+    public virtual string ModifiedUserName { get; set; }
+
+    /// <summary>
+    /// 修改者姓名
+    /// </summary>
+    [Description("修改者姓名")]
+    [Column(Position = -11), MaxLength(60)]
+    [JsonProperty(Order = 10001)]
+    [JsonPropertyOrder(10001)]
+    public virtual string ModifiedUserRealName { get; set; }
+
+    /// <summary>
+    /// 修改时间
+    /// </summary>
+    [Description("修改时间")]
+    [JsonProperty(Order = 10002)]
+    [JsonPropertyOrder(10002)]
+    [Column(Position = -10)]
+    [ServerTime(CanInsert = false, CanUpdate = true)]
+    public virtual DateTime? ModifiedTime { get; set; }
+}
