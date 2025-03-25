9347dce445accee6e0a0381e37e59f51258f183b
zhontai <361243234@qq.com>
2025-03-25 20:35:44 +0800

    优化my-json-editor组件，支持options选项

--- ui/zhontai.ui.admin.vue3/src/components/my-json-editor/index.vue
+++ ui/zhontai.ui.admin.vue3/src/components/my-json-editor/index.vue
@@ -6,8 +6,10 @@
 import { ref, onMounted, onBeforeUnmount } from 'vue'
 import JSONEditor from 'jsoneditor'
 import 'jsoneditor/dist/jsoneditor.min.css'
+import { merge } from 'lodash-es'
 
 const json = defineModel({ type: String })
+const customOptions = defineModel('options', { type: Object })
 
 const editor = ref(null)
 let jsonEditor = null as any
@@ -17,11 +19,17 @@ const get = () => {
 }
 
 onMounted(() => {
-  const options = {
-    mode: 'preview',
-    modes: ['preview', 'code', 'text'],
-    onChange: () => {},
-  }
+  const options = merge(
+    {
+      mode: 'code',
+      modes: ['code', 'code', 'text'],
+      onChange: () => {},
+      mainMenuBar: true,
+      statusBar: true,
+    },
+    customOptions.value || {}
+  )
+
   jsonEditor = new JSONEditor(editor.value, options)
   jsonEditor.set(JSON.parse((json.value || '{}') as string))
 })
