0f25be01daa50a6e8eb7ac17f63e05d244ea7c96
zhontai <361243234@qq.com>
2025-05-27 22:47:36 +0800

    生成前端查询模板接口

---
+++ ui/zhontai.ui.admin.vue3/src/api/admin/SearchTemplate.ts
@@ -0,0 +1,132 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-nocheck
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+import { AxiosResponse } from 'axios'
+import {
+  ResultOutputInt64,
+  ResultOutputListSearchTemplateGetListOutput,
+  ResultOutputSearchTemplateGetUpdateOutput,
+  SearchTemplateAddInput,
+  SearchTemplateUpdateInput,
+} from './data-contracts'
+import { ContentType, HttpClient, RequestParams } from './http-client'
+
+export class SearchTemplateApi<SecurityDataType = unknown> extends HttpClient<SecurityDataType> {
+  /**
+   * No description
+   *
+   * @tags search-template
+   * @name Get
+   * @summary 查询
+   * @request GET:/api/admin/search-template/get
+   * @secure
+   */
+  get = (
+    query?: {
+      /** @format int64 */
+      id?: number
+    },
+    params: RequestParams = {}
+  ) =>
+    this.request<ResultOutputSearchTemplateGetUpdateOutput, any>({
+      path: `/api/admin/search-template/get`,
+      method: 'GET',
+      query: query,
+      secure: true,
+      format: 'json',
+      ...params,
+    })
+  /**
+   * No description
+   *
+   * @tags search-template
+   * @name GetList
+   * @summary 查询列表
+   * @request GET:/api/admin/search-template/get-list
+   * @secure
+   */
+  getList = (
+    query?: {
+      /** @format int64 */
+      moduleId?: number
+    },
+    params: RequestParams = {}
+  ) =>
+    this.request<ResultOutputListSearchTemplateGetListOutput, any>({
+      path: `/api/admin/search-template/get-list`,
+      method: 'GET',
+      query: query,
+      secure: true,
+      format: 'json',
+      ...params,
+    })
+  /**
+   * No description
+   *
+   * @tags search-template
+   * @name Add
+   * @summary 新增
+   * @request POST:/api/admin/search-template/add
+   * @secure
+   */
+  add = (data: SearchTemplateAddInput, params: RequestParams = {}) =>
+    this.request<ResultOutputInt64, any>({
+      path: `/api/admin/search-template/add`,
+      method: 'POST',
+      body: data,
+      secure: true,
+      type: ContentType.Json,
+      format: 'json',
+      ...params,
+    })
+  /**
+   * No description
+   *
+   * @tags search-template
+   * @name UpdateTemplate
+   * @summary 修改
+   * @request PUT:/api/admin/search-template/update-template
+   * @secure
+   */
+  updateTemplate = (data: SearchTemplateUpdateInput, params: RequestParams = {}) =>
+    this.request<AxiosResponse, any>({
+      path: `/api/admin/search-template/update-template`,
+      method: 'PUT',
+      body: data,
+      secure: true,
+      type: ContentType.Json,
+      ...params,
+    })
+  /**
+   * No description
+   *
+   * @tags search-template
+   * @name Delete
+   * @summary 彻底删除
+   * @request DELETE:/api/admin/search-template/delete
+   * @secure
+   */
+  delete = (
+    query?: {
+      /** @format int64 */
+      id?: number
+    },
+    params: RequestParams = {}
+  ) =>
+    this.request<AxiosResponse, any>({
+      path: `/api/admin/search-template/delete`,
+      method: 'DELETE',
+      query: query,
+      secure: true,
+      ...params,
+    })
+}
