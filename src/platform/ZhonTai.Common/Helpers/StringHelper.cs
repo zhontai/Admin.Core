8d9a6ef8d99ad1827bfbf4a180ab67df1b268b76
TT <514263710@qq.com>
2026-07-13 16:14:55 +0800

    生成加密安全随机字符串，默认32位（使用 RandomNumberGenerator）

--- src/platform/ZhonTai.Common/Helpers/StringHelper.cs
+++ src/platform/ZhonTai.Common/Helpers/StringHelper.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography;
 
 namespace ZhonTai.Common.Helpers;
 
@@ -10,30 +10,45 @@ public partial class StringHelper
     private static readonly string _chars = "0123456789";
     private static readonly char[] _constant = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
+
     /// <summary>
-    /// 生成随机字符串，默认32位
+    /// 生成加密安全随机字符串，默认32位（使用 RandomNumberGenerator）
     /// </summary>
     /// <param name="length">随机数长度</param>
     /// <returns></returns>
     public static string GenerateRandom(int length = 32)
     {
-        var newRandom = new StringBuilder();
-        var rd = new Random();
+        var data = new byte[length * 4];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(data);
+
+        var result = new char[length];
         for (int i = 0; i < length; i++)
         {
-            newRandom.Append(_constant[rd.Next(_constant.Length)]);
+            // 每次取4个字节转为无偏索引，避免取模偏差
+            uint value = BitConverter.ToUInt32(data, i * 4);
+            result[i] = _constant[(int)(value % _constant.Length)];
         }
-        return newRandom.ToString();
+        return new string(result);
     }
 
     /// <summary>
-    /// 生成随机6位数
+    /// 生成加密安全随机数字串，默认6位（使用 RandomNumberGenerator）
     /// </summary>
     /// <param name="length"></param>
     /// <returns></returns>
     public static string GenerateRandomNumber(int length = 6)
     {
-        var random = new Random();
-        return new string(Enumerable.Repeat(_chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        var data = new byte[length * 4];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(data);
+
+        var result = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            uint value = BitConverter.ToUInt32(data, i * 4);
+            result[i] = _chars[(int)(value % _chars.Length)];
+        }
+        return new string(result);
     }
 }
